02135c5adc2fd904500367f381b5136f45a82b88
TJWilson92 <wislong@hotmail.co.uk>
2015-12-16 18:22:22 +0000

    Create new question page

---
+++ public/javascripts/questions/new_question_jsme.js
@@ -0,0 +1,9 @@
+function jsmeOnLoad() {
+
+    jsmeApplet = new JSApplet.JSME("jsme_container", "500px", "500px", {
+      "options": "oldlook, star"
+    });
+
+    console.log('successfully loaded new questions');
+
+}
