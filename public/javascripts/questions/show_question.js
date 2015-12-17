b3bfe69bee2c6ecafda47daaf9726c489215041f
TJWilson92 <tw4g10@soton.ac.uk>
2015-12-17 09:09:32 +0000

    Set up create answer form in question/view page
    TODO: Set up routes for creating answer

--- public/javascripts/questions/show_question.js
+++ public/javascripts/questions/show_question.js
@@ -8,4 +8,21 @@ function jsmeOnLoad() {
     console.log(question_molecule);
     jsmeApplet.readMolecule(question_molecule.toString());
 
+    document.getElementById("resetMoleculeButton").onclick = function () {
+      jsmeApplet.readMolecule(question_molecule);
+    };
+
+    document.getElementById("take_jme").onclick = function () {
+      formula = jsmeApplet.smiles();
+      formula_jme = jsmeApplet.jmeFile();
+      if (formula !== "") {
+          document.getElementById("AnswerMolecule_smiles_input").value = formula;
+          document.getElementById("AnswerMolecule_jme_input").value = formula_jme;
+          document.getElementById("newAnswerSubmitButton").style.display ='block';
+      } else {
+        alert("No molecular structure detected");
+      }
+    }
+
+
 }
